a533e895c64f3fcba5d4e61c232502c67d07ebaf
Kenny Chong <7145331+kennycyb@users.noreply.github.com>
2025-12-01 08:58:50 +0800

    add test agent

---
+++ internal/service/encrypt/encrypt_suite_test.go
@@ -0,0 +1,14 @@
+package encrypt
+package encrypt_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestEncrypt(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Encrypt Suite")
+}
